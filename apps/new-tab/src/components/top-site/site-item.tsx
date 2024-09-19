720a0d98a1dbb33586d8cee6069ff7cd66dff285
panfeng <319874759@qq.com>
2024-09-19 17:30:28 +0800

    update

--- apps/new-tab/src/components/top-site/site-item.tsx
+++ apps/new-tab/src/components/top-site/site-item.tsx
@@ -31,17 +31,17 @@ export const SiteItem: FC<SiteItemProps> = ({ site }) => {
 
 
 	return (<Fragment>
-		<Space className="h-24 p-2 flex flex-col items-center justify-center group/site drop-shadow-md hover:drop-shadow-xl rounded-md cursor-pointer shadow-[rgba(0,0,0,0.2)_0_0_10px] backdrop-blur-[4px] backdrop-saturate-150 hover:shadow-[rgba(255,255,255,0.5)_0_0_10px] hover:backdrop-blur-[16px] bg-light/20 transition-all"
+		<Space className="h-24 p-2 flex flex-col items-center justify-center group/site drop-shadow-md hover:drop-shadow-xl rounded-md cursor-pointer shadow-[rgba(0,0,0,0.2)_0_0_10px] backdrop-blur-[4px] backdrop-saturate-150 hover:shadow-[rgba(255,255,255,0.5)_0_0_10px] hover:backdrop-blur-[16px] bg-light/20 transition-all text-light dark:text-dark"
 			onClick={onSiteClick}
 		>
 			<Space>
-				<History size={16} className="text-light opacity-0 group-hover/site:opacity-100 transition-all" />
+				<History size={16} className=" opacity-0 group-hover/site:opacity-100 transition-all" />
 				<Favicon src={site.url} title={site.title} className="rounded-md" />
-				<Button asChild className="p-0 w-fit h-fit bg-transparent hover:bg-black/20" onClick={fixedSite}>
-					<Pin size={16} className="text-light opacity-0 group-hover/site:opacity-100 transition-all" />
+				<Button asChild className="p-0 w-fit h-fit bg-transparent text-light dark:text-dark hover:bg-black/20" onClick={fixedSite}>
+					<Pin size={16} className="opacity-0 group-hover/site:opacity-100 transition-all" />
 				</Button>
 			</Space>
-			<p className="max-w-[100%] transition-all group-site:text-light leading-7 group-hover/site:text-xl truncate">{site.title}</p>
+			<p className="max-w-[100%] transition-all leading-7 group-hover/site:text-xl truncate">{site.title}</p>
 		</Space>
 	</Fragment>);
 };
