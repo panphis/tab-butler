2f68c5ac8054d04b615673495357bf057d3fec0c
panfeng <319874759@qq.com>
2024-10-22 14:41:58 +0800

    fix(popup): bing 搜索打开弹框修复

--- apps/chrome-extension/lib/scriptCreateBookMark.ts
+++ apps/chrome-extension/lib/scriptCreateBookMark.ts
@@ -5,15 +5,18 @@
   }
   const iframe = document.createElement('iframe');
   iframe.id = 'extensions_bookmark_create';
-  iframe.style.border = `none`
-  iframe.style.position = `fixed`
-  iframe.style.top = `10px`
-  iframe.style.right = `10px`
-  iframe.style.width = `600px`
-  iframe.style.height = `480px`
-  iframe.style.zIndex = `2147483647`
-  iframe.style.boxShadow = `0 1px 6px 0 rgba(32,33,36,0.28)`
-  iframe.style.borderRadius = `4px`
+  iframe.style.border = `none`;
+  iframe.style.position = `fixed`;
+  iframe.style.top = `10px`;
+  iframe.style.right = `10px`;
+  iframe.style.width = `600px`;
+  iframe.style.height = `480px`;
+  iframe.style.zIndex = `2147483647`;
+  iframe.style.boxShadow = `0 1px 6px 0 rgba(32,33,36,0.28)`;
+  iframe.style.borderRadius = `4px`;
   iframe.src = chrome.runtime.getURL('/pages/bookmark-popup.html');
-  document.body.appendChild(iframe);
+  // bing 不能直接插入 iframe
+  const div = document.createElement('div');
+  div.appendChild(iframe);
+  document.body.appendChild(div);
 })();
