fdad6cd4ad637c85ba5da7b7a7c5e0a87ad3b0f5
孤舟江海寄余生 <319874759@qq.com>
2025-03-10 21:44:29 +0800

    update

--- apps/web-site/src/app/layout.tsx
+++ apps/web-site/src/app/layout.tsx
@@ -6,48 +6,51 @@ import './globals.css'
 import { ReactNode } from 'react'
 
 export const metadata = {
-  // Define your metadata here
-  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+	// Define your metadata here
+	// For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
 
 const navbar = (
-  <Navbar
-    logo={<b>Nextra</b>}
-  // ... Your additional navbar options
-  />
+	<Navbar
+		logo={<b>Nextra</b>}
+	// ... Your additional navbar options
+	/>
 )
 const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>
 
 
 type RootLayoutProps = {
-  children: ReactNode
+	children: ReactNode
 }
 export default async function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <html
-      // Not required, but good for SEO
-      lang="en"
-      // Required to be set
-      dir="ltr"
-      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
-      suppressHydrationWarning
-    >
-      <Head
-      // ... Your additional head options
-      >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
-      </Head>
-      <body className='min-h-screen'>
-        <Layout
-          navbar={navbar}
-          pageMap={await getPageMap()}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
-          footer={footer}
-        // ... Your additional layout options
-        >
-          {children} 
-        </Layout>
-      </body>
-    </html>
-  )
+	return (
+		<html
+			// Not required, but good for SEO
+			lang="en"
+			// Required to be set
+			dir="ltr"
+			// Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+			suppressHydrationWarning
+		>
+			<Head
+			// ... Your additional head options
+			>
+				{/* Your additional tags should be passed as `children` of `<Head>` element */}
+			</Head>
+
+			<body className='min-h-screen'>
+				<Layout
+					nextThemes={{ attribute: 'class', defaultTheme: 'system', disableTransitionOnChange: true, storageKey: 'theme' }}
+					sidebar={{ autoCollapse: true }}
+					navbar={navbar}
+					pageMap={await getPageMap()}
+					docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+					footer={footer}
+				// ... Your additional layout options
+				>
+					{children}
+				</Layout>
+			</body>
+		</html>
+	)
 }
